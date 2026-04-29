74bf381d7409218d2661d4a62b1f91a979485ec6
chuchito09 <tu_correo_de_github@correo.com>
2026-04-29 01:28:47 -0400

    fix angular refresh using hash routing

--- web-frontend/src/app/app.config.ts
+++ web-frontend/src/app/app.config.ts
@@ -1,12 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+/*import { provideRouter } from '@angular/router';*/
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
-    provideHttpClient()
+
+    provideRouter(routes, withHashLocation())/*,
+   /* provideHttpClient()*/
   ]
 };
